7345aa44499bfb024d2a396290552a661ff12cc4
Than Quoc Chung <quocchung.than@gmail.com>
2023-07-29 22:41:18 +0700

    input

--- index.ts
+++ index.ts
@@ -1,1 +1,13 @@
-"" 
+import { PromptFactory } from './commands/prompt.factory';
+
+// Boostrapping
+const promptFactory = new PromptFactory();
+
+// App Start
+const startTimeParamName = 'since';
+const dateInput = promptFactory
+  .acceptArgument(startTimeParamName)
+  .extract()
+  .find((x) => x.argName == startTimeParamName)?.value;
+
+console.log(dateInput);
