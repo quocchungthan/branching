df65e90dcb7ab21f9f85984c80af1a18920e0f88
Than Quoc Chung <quocchung.than@gmail.com>
2023-07-29 23:14:10 +0700

    fix ts issue

--- commands/printer.factory.ts
+++ commands/printer.factory.ts
@@ -1,9 +1,9 @@
 export interface IPrinter {
-  printTable<T>(data: T[]);
+  printTable<T>(data: T[]): void;
 }
 
 export class Printer implements IPrinter {
-  printTable<T>(data: T[]) {
+  printTable<T>(data: T[]): void {
     console.table(data);
   }
 }
